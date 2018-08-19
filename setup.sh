bce96cef2c768443fc660e445ec61694028b2fa3
Åsmund Eek <aasm.eek@gmail.com>
2018-08-19 15:54:36 +0200

    Updated installation for vscode

--- setup.sh
+++ setup.sh
@@ -7,14 +7,22 @@ cd $WD
 sudo apt update
 sudo apt upgrade -y
 
+# Make bash case insensitive for auto complete
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+echo 'set completion-ignore-case On' >> ~/.inputrc
+
 # Install google chrome
-sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get install libxss1 libappindicator1 libindicator7 -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P $WD
 sudo dpkg -i google-chrome*.deb
 
 # Install vscode
-wget https://code.visualstudio.com/docs/?dv=linux64_deb -P $WD
-sudo dpkg -i code_*.deb
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code
 
 # Install spotify
 snap install spotify --classic
@@ -40,5 +48,5 @@ chmod +x gnome-shell-extension-installer
 ./gnome-shell-extension-installer 8 --yes       # PlacesStatusIndicator
 
 # Install common developing tools
-sudo apt install cmake make -y
+sudo apt install cmake make vim -y
 
